a3125ef7ce73e06ac2d6e610aca48652a698f5fc
kushankavinda <kaavinda92@gmail.lk>
2018-01-09 14:39:11 +0530

    //analys with Quartile1 values

--- src/Reader/ReadExcel.java
+++ src/Reader/ReadExcel.java
@@ -8,6 +8,8 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import jxl.Cell;
 import jxl.CellType;
@@ -16,7 +18,7 @@ import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class ReadExcel {
-	private String inputFile;
+	private static String inputFile;
 
 	public void setInputFile(String inputFile) {
 		this.inputFile = inputFile;
@@ -24,52 +26,45 @@ public class ReadExcel {
 	public void same_video_id() throws BiffException, IOException {
 		File inputWorkbook = new File(inputFile);
 		Workbook w1 = Workbook.getWorkbook(inputWorkbook); // Get the first sheet
-		Sheet sheet = w1.getSheet(0);
-		
-		//ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+		Sheet sheet = w1.getSheet(0);	
 		String video_id,video_id_check;
-		int count_of_video_id=0;
-		for(int i=1;i<2/*sheet.getRows()*/; i++) {
+		int count_of_video_id;
+		List<String> mylist = new ArrayList<String>();
+		int simmilar_id_checker;
+		for(int i=1;i<sheet.getRows(); i++) {
+			count_of_video_id=0;
 			Cell cell = sheet.getCell(0, i);
 			video_id_check = cell.getContents();
-			
-			///for(int k=0;k<list.size();k++) {
-				
-				//if(!(list.get(k).equals(video_id_check))) {
+			simmilar_id_checker=0;		
 					for(int j=2;j<sheet.getRows();j++) {
 						Cell cell1 = sheet.getCell(0, j);
-						video_id = cell1.getContents();
-						
-						if(video_id_check.equals(video_id)) {
-							//list.add(video_id_check);//Adding object in arraylist  
-							++count_of_video_id;
-							System.out.println("this id has equal ids : "+ (j+1));
+						video_id = cell1.getContents();	
+						for(int k=0;k<mylist.size();k++) {
+							if(video_id.equals(mylist.get(k))){
+								++simmilar_id_checker;
+							}
 						}
-						
+						if(simmilar_id_checker==0) {					
+							if(video_id_check.equals(video_id)) { 
+								mylist.add(video_id);							
+								++count_of_video_id;
+								System.out.println("this id has equal ids : "+ (j+1));
+							}
+						}
 					}
-				//}
-			//}
-					System.out.println("video_id"+ " " +video_id_check+ "has nu of times : "+count_of_video_id);
+		System.out.println("video_id"+ " " +video_id_check+ "has nu of times : "+count_of_video_id);
 		}
 	}
 
-	public String[] trend_videos() throws BiffException, IOException {
+	public String[] trend_videos(int quartile_1_of_view,int quartile_1_of_likes,int quartile_1_of_dislikes,int quartile_1_of_comments) throws BiffException, IOException {
 		File inputWorkbook = new File(inputFile);
 		Workbook w = Workbook.getWorkbook(inputWorkbook); // Get the first sheet
 		Sheet sheet = w.getSheet(0);
-		
-		/*
-		 * //get current date and time DateTimeFormatter dtf =
-		 * DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); LocalDateTime now =
-		 * LocalDateTime.now(); System.out.println("local date"+dtf.format(now));
-		 * //2016/11/16 12:08:43
-		 */
 
-		String number_of_views_string_value, nu_likes_string_value, nu_dislikes_string_value, video_id;
-		int number_of_views, nu_likes, nu_dislikes;
-        
-		
-		for (int i = 2; i < sheet.getRows(); i++) {
+		String number_of_views_string_value, nu_likes_string_value, nu_dislikes_string_value, video_id,commnet_string_value;
+		int number_of_views, nu_likes, nu_dislikes,nu_of_comment;
+        		
+		for (int i = 1; i < sheet.getRows(); i++) {
 			Cell cell1 = sheet.getCell(5, i);
 			
 			try {
@@ -77,9 +72,9 @@ public class ReadExcel {
 						
 				number_of_views_string_value = cell1.getContents();
 				number_of_views = Integer.parseInt(number_of_views_string_value);
-				//System.out.println("number_of_views : "+number_of_views);	
-				// videos view <10000
-					if (number_of_views<10000) {
+				
+				// videos view > quartile_1_of_view
+					if (number_of_views>quartile_1_of_view) {
 						Cell cell2 = sheet.getCell(6, i);
 						nu_likes_string_value = cell2.getContents();
 						nu_likes = Integer.parseInt(nu_likes_string_value);
@@ -88,11 +83,17 @@ public class ReadExcel {
 						nu_dislikes_string_value = cell3.getContents();
 						nu_dislikes = Integer.parseInt(nu_dislikes_string_value);
 						
-						//analys likes and dislikes
-						if (nu_likes > 10 * nu_dislikes) {
-							Cell cell4 = sheet.getCell(0, i);
-							video_id = cell4.getContents();
-							System.out.println("this is a trending video_id : " + video_id);
+				//analys likes and dislikes with Quartile1 values
+						if (nu_likes > quartile_1_of_likes  && quartile_1_of_dislikes > nu_dislikes) {
+							Cell cell4 = sheet.getCell(8, i);
+							commnet_string_value = cell4.getContents();
+							nu_of_comment = Integer.parseInt(commnet_string_value);
+				//analys comment with Quartile1 values			
+							if(nu_of_comment >quartile_1_of_comments) {
+								Cell cell5 = sheet.getCell(0, i);
+								video_id = cell5.getContents();
+								System.out.println("this is a trending video_id : " + video_id);
+							}
 						}		
 					}
 			} catch (Exception e) {
@@ -101,12 +102,40 @@ public class ReadExcel {
 		}
 		return null;
 	}
-
+	public static int quartile_1(int j) throws BiffException, IOException {
+		File inputWorkbook = new File(inputFile);
+		Workbook w = Workbook.getWorkbook(inputWorkbook); // Get the first sheet
+		Sheet sheet = w.getSheet(0);
+		
+		String number_of_views_string_value ;
+		int number_of_views,quartile_1;
+		int array[]=new int[sheet.getRows()];
+		for(int i = 1; i < sheet.getRows(); i++) {
+			Cell cell1 = sheet.getCell(j,i);
+			CellType type = cell1.getType(); 
+						
+			number_of_views_string_value = cell1.getContents();
+			number_of_views = Integer.parseInt(number_of_views_string_value);
+			array[i-1]=number_of_views;
+		}
+		Arrays.sort(array);
+		quartile_1=array[sheet.getRows()/4];
+		
+		System.out.println("quartile_1 : "+quartile_1);
+		return quartile_1;
+	}
 	public static void main(String[] args) throws IOException, BiffException {
 		ReadExcel test = new ReadExcel();
 		test.setInputFile("C:\\eclips for java SE\\GBvideos.XLS");
-		//test.trend_videos();
-		test.same_video_id();
+		
+		int quartile_1_of_view=quartile_1(5);
+		int quartile_1_of_likes=quartile_1(6);
+		int quartile_1_of_dislikes=quartile_1(7);
+		int quartile_1_of_comments=quartile_1(8);
+		
+		test.trend_videos(quartile_1_of_view,quartile_1_of_likes,quartile_1_of_dislikes,quartile_1_of_comments);
+		
+		//test.same_video_id();
 		// test.read();
 	}
 }
